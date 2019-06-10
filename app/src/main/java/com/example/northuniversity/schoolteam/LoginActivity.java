784614d61371e093dacac2bace400ef5e7633add
240484406@qq.com <240484406@qq.com>
2019-06-10 22:05:06 +0800

    登陆界面实现

---
+++ app/src/main/java/com/example/northuniversity/schoolteam/LoginActivity.java
@@ -0,0 +1,142 @@
+package com.example.northuniversity.schoolteam;
+
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.animation.ValueAnimator;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView mBtnLogin;
+
+    private View progress;
+
+    private View mInputLayout;
+
+    private float mWidth, mHeight;
+
+    private LinearLayout mName, mPsw;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_login);
+
+        initView();
+    }
+
+    private void initView() {
+        mBtnLogin = (TextView) findViewById(R.id.main_btn_login);
+        progress = findViewById(R.id.layout_progress);
+        mInputLayout = findViewById(R.id.input_layout);
+        mName = (LinearLayout) findViewById(R.id.input_layout_name);
+        mPsw = (LinearLayout) findViewById(R.id.input_layout_psw);
+
+        mBtnLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        // 计算出控件的高与宽
+        mWidth = mBtnLogin.getMeasuredWidth();
+        mHeight = mBtnLogin.getMeasuredHeight();
+        // 隐藏输入框
+        mName.setVisibility(View.INVISIBLE);
+        mPsw.setVisibility(View.INVISIBLE);
+
+        inputAnimator(mInputLayout, mWidth, mHeight);
+
+    }
+
+    /**
+     * 输入框的动画效果
+     *
+     * @param view 控件
+     * @param w    宽
+     * @param h    高
+     */
+    private void inputAnimator(final View view, float w, float h) {
+
+        AnimatorSet set = new AnimatorSet();
+
+        ValueAnimator animator = ValueAnimator.ofFloat(0, w);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float value = (Float) animation.getAnimatedValue();
+                ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) view
+                        .getLayoutParams();
+                params.leftMargin = (int) value;
+                params.rightMargin = (int) value;
+                view.setLayoutParams(params);
+            }
+        });
+
+        ObjectAnimator animator2 = ObjectAnimator.ofFloat(mInputLayout,
+                "scaleX", 1f, 0.5f);
+        set.setDuration(1000);
+        set.setInterpolator(new AccelerateDecelerateInterpolator());
+        set.playTogether(animator, animator2);
+        set.start();
+        set.addListener(new Animator.AnimatorListener() {
+
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                /**
+                 * 动画结束后，先显示加载的动画，然后再隐藏输入框
+                 */
+                progress.setVisibility(View.VISIBLE);
+                progressAnimator(progress);
+                mInputLayout.setVisibility(View.INVISIBLE);
+
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+        });
+
+    }
+
+    /**
+     * 出现进度动画
+     *
+     * @param view
+     */
+    private void progressAnimator(final View view) {
+        PropertyValuesHolder animator = PropertyValuesHolder.ofFloat("scaleX",
+                0.5f, 1f);
+        PropertyValuesHolder animator2 = PropertyValuesHolder.ofFloat("scaleY",
+                0.5f, 1f);
+        ObjectAnimator animator3 = ObjectAnimator.ofPropertyValuesHolder(view,
+                animator, animator2);
+        animator3.setDuration(1000);
+        animator3.setInterpolator(new JellyInterpolator());
+        animator3.start();
+
+    }
+
+}
